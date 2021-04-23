9a74afd10250925529ccda8e86f72c9d99dee0e3
abdelatifDer <31412801+abdelatifDer@users.noreply.github.com>
2021-04-23 14:15:16 +0100

    Update localise.js

--- localise.js
+++ localise.js
@@ -198,18 +198,3 @@ $("#firstbutton").on("click", function () {
     $(".postcode-error").hide();
   }
 });
-
-// update the url params
-if (document.querySelector(".estimate-page")) {
-  console.log("page exists")
-  if (history.pushState && localStorage.getItem("city")) {
-    var newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname +
-      "?city=" +
-      localStorage.getItem("city");
-    window.history.pushState({ path: newurl }, "", newurl);
-  }
-}
