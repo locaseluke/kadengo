33af7af3a40c4e01b6536b00f458a254d043a1fb
abdelatifDer <derkaoui312012@gmail.com>
2021-04-23 13:51:45 +0100

    test changing url params

--- localise.js
+++ localise.js
@@ -198,3 +198,17 @@ $("#firstbutton").on("click", function () {
     $(".postcode-error").css("display", "none");
   }
 });
+
+// update the url params
+if (document.querySelector(".estimate-page")) {
+  if (history.pushState && localStorage.getItem("city")) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname +
+      "?city=" +
+      localStorage.getItem("city");
+    window.history.pushState({ path: newurl }, "", newurl);
+  }
+}
